fcf6018a488eef03e87836a651d8d0a7785f1fbb
Luis Fernando Rojas González <luisfernando.lzc@hotmail.com>
2023-05-06 00:30:10 -0600

    Agregué el API de Github
    Ahora es posible obtener datos de los usuarios a través de peticiones HTTP utilizando la API de Github

--- Proyecto/js/index.js
+++ Proyecto/js/index.js
@@ -1,3 +1,4 @@
+import { Octokit } from 'https://cdn.skypack.dev/octokit';
 import { mostrarCuentas } from './modules/cuentas.js';
 import { mostrarEstadisticas } from './modules/estadisticas.js';
 
@@ -16,3 +17,20 @@ btnCuentas.addEventListener('click', async () => {
     mainPage.innerHTML = '';
     mainPage.appendChild(await mostrarCuentas());
 });
+
+const octokit = new Octokit({
+    auth: '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'
+});
+
+/*
+const request = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+    owner: "octocat",
+    repo: "Spoon-Knife",
+  });
+
+  console.log(request.data);
+  */
+
+const request = await octokit.request("GET /users/LuisFer666/events");
+
+console.log(request.data);
